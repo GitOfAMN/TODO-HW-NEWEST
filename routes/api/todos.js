3f4db2767a8b9698ef6c7108fd664128f889cb0c
GitOfAMN <ashleynickens@gmail.com>
2023-01-02 22:43:16 -0500

    CRUD functionality working

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -4,14 +4,19 @@ const todoCtrl = require('../../controllers/api/todos')
 
 // Index for the /api/todos path
 router.get('/', todoCtrl.indexNotComplete, todoCtrl.jsonTodos)
+
 // Index for the /api/todos/completed path
 router.get('/completed', todoCtrl.indexComplete, todoCtrl.jsonTodos)
+
 // Delete for the /api/todos/:id path
 router.delete('/:id', todoCtrl.destroy, todoCtrl.jsonTodo)
+
 // Update for the /api/todo/:id path
 router.put('/:id', todoCtrl.update, todoCtrl.jsonTodo)
+
 // Create for the /api/todos path
 router.post('/', todoCtrl.create, todoCtrl.jsonTodo)
+
 // Show for the /api/todos/:id path
 router.get('/:id', todoCtrl.show, todoCtrl.jsonTodo)
 
